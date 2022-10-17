f809d4c87bece9ff0dcf995f8656cc38d172b50a
Sampleproject <amejia9149@bths.edu>
2022-10-16 20:39:34 -0400

    added runner.java. Converted int to string

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,25 +1,50 @@
 import java.math.MathContext;
+
+import static java.lang.System.out;
+
 public class LinearEquation {
-  private int  x1;
-  private int x2;
-  private int y1;
-  private int y2;
-  private double yIntercept;
-  private double distance;
-  private double slope;
-  private double slopey;
-  public LinearEquation(int x1,int x2, int y1, int y2){
-      this.x1=x1; this.x2=x2; this.y1 = y1; this.y2=y2;
-      slope=(y2-y1);  slopey=(x2-x1);
-      System.out.println(slope/slopey);
-      int yIntercept=  (int)(x1*-(slope/slopey)+y1);
-      System.out.println(yIntercept);
+    private int x1;
+    private String pairOne;
+    private String pairTwo;
+    private String Equation;
+    private int x2;
+    private int y1;
+    private int y2;
+    private int yIntercept;
+    private double distance;
+    private double slope;
+    private double slopey;
+    public LinearEquation(String x1, String x2, String y1, String y2) {
+        this.x1 = Integer.parseInt(x1);
+        this.x2 = Integer.parseInt(x2);
+        this.y1 = Integer.parseInt(y1);
+        this.y2 = Integer.parseInt(y2);
+        pairOne = "(" + x1 + "," + y1 + ")";
+        pairTwo = "(" + x2 + "," + y2 + ")";
+        slope = (this.y2-this.y1);
+        slopey = (this.x2 - this.x1);
+        //System.out.println(slope /slopey);//
+        yIntercept = (int) (Integer.parseInt(x1) * -(slope / slopey) + Integer.parseInt(y1));
+        Equation = "Slope intercept form: y= "+(slope/slopey)+"x"+"+"+yIntercept;
+
+    }
+
+    public String distance() {
+        distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return "Distance between points:"+distance;
+
+    }
+
+    public String toString() {
+       String pairs = "First Pair:" + pairOne+ "\n";
+       pairs += "Second Pair" + pairTwo + "\n";
+       pairs += "Slope of line:"+(slope / slopey)+"\n";
+       pairs += "Y-intercept:"+ yIntercept+"\n";
+       pairs += Equation+"\n";
+       pairs += distance;
+
+        return pairs;
 
 
-  }
-  public double distance(){
-      distance= Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-      return distance;
-  }
-
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,22 @@
 import javax.sound.sampled.Line;
-
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
-        LinearEquation new1= new LinearEquation(2,3,5,6);
-        System.out.println(new1.distance());
-
+        Scanner s = new Scanner(System.in);
+        String xyPoint;
+        String xyPointTwo;
+        System.out.print("Enter the first x-y coordinate point: ");
+        xyPoint= s.nextLine();
+        String[] parts = xyPoint.split(",");
+        String x= parts[0].trim().substring(1).trim();
+        String y = parts[1].trim().substring(0,parts[1].trim().length()-1).trim();
+        System.out.println();
+        System.out.print("Enter the second x-y coordinate point: ");
+        xyPointTwo = s.nextLine();
+        String[] partsTwo = xyPointTwo.split(",");
+        String x2= partsTwo[0].trim().substring(1).trim();
+        String y2 = partsTwo[1].trim().substring(0,parts[1].trim().length()-1).trim();
+        LinearEquation Equation1= new LinearEquation(x,x2,y,y2);
+        System.out.println(Equation1.toString());
     }
 }
