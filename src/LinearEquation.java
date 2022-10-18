942981afa3d4d0f06e3dbc3f8dd6c629eb223301
Sampleproject <amejia9149@bths.edu>
2022-10-18 07:27:17 -0400

    converted string into int. Changed yIntercept formula. Added loop to check if yIntercept is negative.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,50 +1,60 @@
 import java.math.MathContext;
+import java.text.DecimalFormat;
 
 import static java.lang.System.out;
 
 public class LinearEquation {
+    DecimalFormat dRound= new DecimalFormat("0.00");
     private int x1;
     private String pairOne;
     private String pairTwo;
     private String Equation;
     private int x2;
+    private double xValue;
+    private String newPoint;
     private int y1;
     private int y2;
-    private int yIntercept;
+    private double yIntercept;
     private double distance;
     private double slope;
-    private double slopey;
-    public LinearEquation(String x1, String x2, String y1, String y2) {
-        this.x1 = Integer.parseInt(x1);
-        this.x2 = Integer.parseInt(x2);
-        this.y1 = Integer.parseInt(y1);
-        this.y2 = Integer.parseInt(y2);
+    public LinearEquation(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
         pairOne = "(" + x1 + "," + y1 + ")";
         pairTwo = "(" + x2 + "," + y2 + ")";
-        slope = (this.y2-this.y1);
-        slopey = (this.x2 - this.x1);
-        //System.out.println(slope /slopey);//
-        yIntercept = (int) (Integer.parseInt(x1) * -(slope / slopey) + Integer.parseInt(y1));
-        Equation = "Slope intercept form: y= "+(slope/slopey)+"x"+"+"+yIntercept;
+        slope =((y2-y1)/(x2-x1));
+        yIntercept = (double) (this.x1 * -(slope) + this.y1);
+        if (yIntercept>=0.00){
+            Equation = "Slope intercept form: y= "+slope+"x"+"+"+yIntercept;
+
+        }else {
+            Equation = "Slope intercept form: y= "+slope+"x"+yIntercept;
+
+        }
 
     }
-
     public String distance() {
-        distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        return "Distance between points:"+distance;
+        distance = Math.sqrt((Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
+        return dRound.format(distance);
 
     }
-
     public String toString() {
        String pairs = "First Pair:" + pairOne+ "\n";
        pairs += "Second Pair" + pairTwo + "\n";
-       pairs += "Slope of line:"+(slope / slopey)+"\n";
-       pairs += "Y-intercept:"+ yIntercept+"\n";
+       pairs += "Slope of line:"+(slope)+"\n";
+       pairs += "Y-intercept:"+ dRound.format(yIntercept)+"\n";
        pairs += Equation+"\n";
-       pairs += distance;
+       pairs += "Distance between points:"+distance()+"\n";
+       pairs+= "Solved coordinate point is:"+ newPoint;
 
         return pairs;
-
-
+    }
+    public String coordinatePoint(String xValue){
+        this.xValue= Integer.parseInt(xValue);
+        double valueY= (slope*this.xValue)+yIntercept;
+        String newPoint = "("+this.xValue+","+valueY+")";
+        return newPoint;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String xyPoint;
-        String xyPointTwo;
         System.out.print("Enter the first x-y coordinate point: ");
-        xyPoint= s.nextLine();
-        String[] parts = xyPoint.split(",");
-        String x= parts[0].trim().substring(1).trim();
-        String y = parts[1].trim().substring(0,parts[1].trim().length()-1).trim();
-        System.out.println();
+        String xyPoint = s.nextLine();
+        int x1 = Integer.parseInt(xyPoint.substring(xyPoint.indexOf("(") + 1, xyPoint.indexOf(",")));
+        int y1 = Integer.parseInt(xyPoint.substring(xyPoint.indexOf(",") + 1, xyPoint.indexOf(")")));
         System.out.print("Enter the second x-y coordinate point: ");
-        xyPointTwo = s.nextLine();
-        String[] partsTwo = xyPointTwo.split(",");
-        String x2= partsTwo[0].trim().substring(1).trim();
-        String y2 = partsTwo[1].trim().substring(0,parts[1].trim().length()-1).trim();
-        LinearEquation Equation1= new LinearEquation(x,x2,y,y2);
-        System.out.println(Equation1.toString());
-    }
-}
+        String xyPointTwo = s.nextLine();
+        int x2 = Integer.parseInt(xyPointTwo.substring(xyPointTwo.indexOf("(") + 1, xyPointTwo.indexOf(",")));
+        int y2 = Integer.parseInt(xyPointTwo.substring(xyPointTwo.indexOf(",") + 1, xyPointTwo.indexOf(")")));
+        LinearEquation Equation = new LinearEquation(x1,x2,y1,y2);
+       System.out.println(Equation.toString());
+    }}
