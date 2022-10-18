0328332e62289741965d93ab7b8a62e3b05116ad
BT_4N2_15 <amejia9149@bths.edu>
2022-10-18 12:33:19 -0400

    added distance method

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,8 +1,4 @@
-import java.math.MathContext;
 import java.text.DecimalFormat;
-
-import static java.lang.System.out;
-
 public class LinearEquation {
     DecimalFormat dRound= new DecimalFormat("0.00");
     private int x1;
@@ -11,11 +7,10 @@ public class LinearEquation {
     private String Equation;
     private int x2;
     private double xValue;
-    private String newPoint;
+    private double newPoint;
     private int y1;
     private int y2;
     private double yIntercept;
-    private double distance;
     private double slope;
     public LinearEquation(int x1, int x2, int y1, int y2) {
         this.x1 = x1;
@@ -25,7 +20,7 @@ public class LinearEquation {
         pairOne = "(" + x1 + "," + y1 + ")";
         pairTwo = "(" + x2 + "," + y2 + ")";
         slope =((y2-y1)/(x2-x1));
-        yIntercept = (double) (this.x1 * -(slope) + this.y1);
+        yIntercept = (this.x1 * -(slope) + this.y1);
         if (yIntercept>=0.00){
             Equation = "Slope intercept form: y= "+slope+"x"+"+"+yIntercept;
 
@@ -36,7 +31,7 @@ public class LinearEquation {
 
     }
     public String distance() {
-        distance = Math.sqrt((Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
+        double distance = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
         return dRound.format(distance);
 
     }
@@ -47,14 +42,14 @@ public class LinearEquation {
        pairs += "Y-intercept:"+ dRound.format(yIntercept)+"\n";
        pairs += Equation+"\n";
        pairs += "Distance between points:"+distance()+"\n";
-       pairs+= "Solved coordinate point is:"+ newPoint;
+       pairs+= "Solved coordinate point is: "+ "("+(int)xValue+","+(int)newPoint+")";
 
         return pairs;
     }
-    public String coordinatePoint(String xValue){
-        this.xValue= Integer.parseInt(xValue);
-        double valueY= (slope*this.xValue)+yIntercept;
-        String newPoint = "("+this.xValue+","+valueY+")";
+    public double coordinatePoint(int xValue){
+        this.xValue= xValue;
+        int newPoint=(int)(slope)*(xValue-x1)+y1;
+        this.newPoint= newPoint;
         return newPoint;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -12,5 +12,9 @@ public class Runner {
         int x2 = Integer.parseInt(xyPointTwo.substring(xyPointTwo.indexOf("(") + 1, xyPointTwo.indexOf(",")));
         int y2 = Integer.parseInt(xyPointTwo.substring(xyPointTwo.indexOf(",") + 1, xyPointTwo.indexOf(")")));
         LinearEquation Equation = new LinearEquation(x1,x2,y1,y2);
-       System.out.println(Equation.toString());
+       System.out.print("Enter a new x coordinate:");
+       int newXCoordinate = Integer.parseInt(s.nextLine());
+       Equation.coordinatePoint(newXCoordinate);
+        System.out.println(Equation.toString());
+
     }}
